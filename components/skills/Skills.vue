3a86345df192e6826aa96ebed6133513ba0d180b
Iurii Kyrylovskyi <iurii712@gmail.com>
2024-05-30 15:01:41 +0300

    add skills

--- components/skills/Skills.vue
+++ components/skills/Skills.vue
@@ -4,15 +4,21 @@ import { skills } from '@/utils/constants/skills';
 
 <template>
   <div class="w-full flex items-center justify-center">
-    <ul class="flex gap-2 flex-wrap items-center justify-center shadow-2xl">
-      <li v-for="s in skills" class="skill-item relative cursor-pointer">
-        <p class="absolute">
-          <span
-            class="py-1 px-2 bg-white border-1 rounded text-center shadow-md"
-            >{{ s.name }}</span
-          >
-        </p>
-        <img :src="s.icon" :alt="s.name" />
+    <ul
+      class="flex gap-2 flex-wrap items-center justify-center shadow-2xl py-[50px]"
+    >
+      <li
+        v-for="s in skills"
+        class="skill-item flip-card relative cursor-pointer"
+      >
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img :src="s.icon" :alt="s.name" />
+          </div>
+          <div class="flip-card-back">
+            {{ s.name }}
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -23,19 +29,26 @@ import { skills } from '@/utils/constants/skills';
   background: white;
   border: 5px solid white;
   border-radius: 4px;
+  width: 120px;
+  aspect-ratio: 1 / 1;
+
+  position: relative;
+
+  @media (max-width: 500px) {
+    flex: 1 1 22%;
+    width: 22%;
+  }
 }
 .skill-item img {
-  /* position: absolute; */
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   max-width: 100%;
   object-fit: contain;
-
-  max-width: 120px;
-  max-height: 120px;
 }
+
 p {
   opacity: 0;
   transition: all 0.3s linear;
@@ -50,5 +63,42 @@ p:hover {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.flip-card {
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+@media (hover: hover) {
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
 }
 </style>
--- utils/constants/skills.ts
+++ utils/constants/skills.ts
@@ -8,7 +8,7 @@ import type { ISkill } from '~/interfaces/skill';
 //   typescript,
 //   html,
 //   css,
-//   reactjs,
+//   react,
 //   redux,
 //   tailwind,
 //   nodejs,
@@ -26,24 +26,75 @@ import type { ISkill } from '~/interfaces/skill';
 //   threejs,
 // } from '@/assets/images/skills';
 
-const reactjs = '/reactjs.svg';
-const chargebee = '/chargebee.svg';
-const css = '/css.svg';
-const docusign = '/docusign.svg';
-const gatsby = '/gatsby.svg';
-const html5 = '/html5.svg';
-const js = '/javascript.svg';
-const nest = '/nestjs.svg';
-const next = '/nextjs.svg';
-const reactQuery = '/react-query.svg';
-const redux = '/redux.svg';
-const tailwindcss = '/tailwindcss.svg';
-const ts = '/ts.svg';
+const react = '/skills/react.svg';
+const chargebee = '/skills/chargebee.svg';
+const css = '/skills/css.svg';
+const docusign = '/skills/docusign.svg';
+const gatsby = '/skills/gatsby.svg';
+const html5 = '/skills/html5.svg';
+const js = '/skills/javascript.svg';
+const nest = '/skills/nestjs.svg';
+const next = '/skills/nextjs.svg';
+const reactQuery = '/skills/react-query.svg';
+const redux = '/skills/redux.svg';
+const tailwindcss = '/skills/tailwindcss.svg';
+const ts = '/skills/ts.svg';
+const auth0 = '/skills/auth0.svg';
+const aws = '/skills/aws.svg';
+const directus = '/skills/directus.svg';
+const docker = '/skills/docker.svg';
+const figma = '/skills/figma.svg';
+const formik = '/skills/formik.svg';
+const gDosc = '/skills/g-docs.svg';
+const gMaps = '/skills/g-maps.svg';
+const git = '/skills/git.svg';
+const github = '/skills/github.svg';
+const gitlab = '/skills/gitlab.svg';
+const graphql = '/skills/graphql.svg';
+const gulp = '/skills/gulp.svg';
+const hotjar = '/skills/hotjar.svg';
+const jest = '/skills/jest.svg';
+const jotform = '/skills/jotform.svg';
+const jquery = '/skills/jquery.svg';
+const leaflet = '/skills/leaflet.svg';
+const less = '/skills/less.svg';
+const loom = '/skills/loom.svg';
+const mailchimp = '/skills/mailchimp.svg';
+const markup = '/skills/markup.svg';
+const netlify = '/skills/netlify.svg';
+const nextAuth = '/skills/next-auth.png';
+const nodemailer = '/skills/nodemailer.png';
+const npm = '/skills/npm.svg';
+const nuxt = '/skills/nuxt.svg';
+const playwright = '/skills/playwright.svg';
+const pnpm = '/skills/pnpm.svg';
+const reactTestingLibrary = '/skills/react-testing-library.png';
+const restApi = '/skills/rest-api.svg';
+const sass = '/skills/sass.svg';
+const sentry = '/skills/sentry.svg';
+const sketch = '/skills/sketch.svg';
+const stripe = '/skills/stripe.svg';
+const vercel = '/skills/vercel.svg';
+const vite = '/skills/vite.svg';
+const vsCode = '/skills/vs-code.svg';
+const vueuse = '/skills/vue-use.png';
+const vue = '/skills/vue.svg';
+const webflow = '/skills/webflow.svg';
+const webpack = '/skills/webpack.svg';
+const zustand = '/skills/zustand.png';
 
 export const skills: ISkill[] = [
   {
+    name: 'AWS services',
+    icon: aws,
+  },
+  {
+    name: 'Auth0',
+    icon: auth0,
+  },
+  {
     name: 'React.js',
-    icon: reactjs,
+    icon: react,
   },
   {
     name: 'Chargebee',
@@ -93,122 +144,169 @@ export const skills: ISkill[] = [
     name: 'TypeScript',
     icon: ts,
   },
-  // {
-  //   name: 'HTML 5',
-  //   icon: html,
-  // },
-  // {
-  //   name: 'CSS 3',
-  //   icon: css,
-  // },
-  // {
-  //   name: 'JavaScript',
-  //   icon: javascript,
-  // },
-  // {
-  //   name: 'TypeScript',
-  //   icon: typescript,
-  // },
-  // {
-  //   name: 'React JS',
-  //   icon: reactjs,
-  // },
-  // {
-  //   name: 'Redux Toolkit',
-  //   icon: redux,
-  // },
-  // {
-  //   name: 'Tailwind CSS',
-  //   icon: tailwind,
-  // },
-  // {
-  //   name: 'Node JS',
-  //   icon: nodejs,
-  // },
-  // {
-  //   name: 'MongoDB',
-  //   icon: mongodb,
-  // },
-  // {
-  //   name: 'Three JS',
-  //   icon: threejs,
-  // },
-  // {
-  //   name: 'git',
-  //   icon: git,
-  // },
-  // {
-  //   name: 'figma',
-  //   icon: figma,
-  // },
-  // {
-  //   name: 'docker',
-  //   icon: docker,
-  // },
-];
+  {
+    name: 'Zustand',
+    icon: zustand,
+  },
+  {
+    name: 'Webpack',
+    icon: webpack,
+  },
+  {
+    name: 'Webflow',
+    icon: webflow,
+  },
+  {
+    name: 'Vue',
+    icon: vue,
+  },
+  {
+    name: 'Vue Use',
+    icon: vueuse,
+  },
+  {
+    name: 'VS Code',
+    icon: vsCode,
+  },
+  {
+    name: 'Vite',
+    icon: vite,
+  },
+  {
+    name: 'Vercel',
+    icon: vercel,
+  },
+  {
+    name: 'Stripe',
+    icon: stripe,
+  },
+  {
+    name: 'Sketch',
+    icon: sketch,
+  },
+  {
+    name: 'Sentry',
+    icon: sentry,
+  },
+  {
+    name: 'SASS',
+    icon: sass,
+  },
+  {
+    name: 'REST Api',
+    icon: restApi,
+  },
+  {
+    name: 'React Testing Library',
+    icon: reactTestingLibrary,
+  },
 
-// const projects = [
-//   {
-//     name: 'Car Rent',
-//     description:
-//       'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
-//     tags: [
-//       {
-//         name: 'react',
-//         color: 'blue-text-gradient',
-//       },
-//       {
-//         name: 'mongodb',
-//         color: 'green-text-gradient',
-//       },
-//       {
-//         name: 'tailwind',
-//         color: 'pink-text-gradient',
-//       },
-//     ],
-//     image: carrent,
-//     source_code_link: 'https://github.com/',
-//   },
-//   {
-//     name: 'Job IT',
-//     description:
-//       'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
-//     tags: [
-//       {
-//         name: 'react',
-//         color: 'blue-text-gradient',
-//       },
-//       {
-//         name: 'restapi',
-//         color: 'green-text-gradient',
-//       },
-//       {
-//         name: 'scss',
-//         color: 'pink-text-gradient',
-//       },
-//     ],
-//     image: jobit,
-//     source_code_link: 'https://github.com/',
-//   },
-//   {
-//     name: 'Trip Guide',
-//     description:
-//       'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
-//     tags: [
-//       {
-//         name: 'nextjs',
-//         color: 'blue-text-gradient',
-//       },
-//       {
-//         name: 'supabase',
-//         color: 'green-text-gradient',
-//       },
-//       {
-//         name: 'css',
-//         color: 'pink-text-gradient',
-//       },
-//     ],
-//     image: tripguide,
-//     source_code_link: 'https://github.com/',
-//   },
-// ];
+  {
+    name: 'pnpm',
+    icon: pnpm,
+  },
+  {
+    name: 'Playwright',
+    icon: playwright,
+  },
+  {
+    name: 'Nuxt',
+    icon: nuxt,
+  },
+  {
+    name: 'NPM',
+    icon: npm,
+  },
+  {
+    name: 'Nodemailer',
+    icon: nodemailer,
+  },
+  {
+    name: 'NextAuth',
+    icon: nextAuth,
+  },
+  {
+    name: 'Netlify',
+    icon: netlify,
+  },
+  {
+    name: 'Markup',
+    icon: markup,
+  },
+  {
+    name: 'Mailchimp',
+    icon: mailchimp,
+  },
+  {
+    name: 'LOOM',
+    icon: loom,
+  },
+  {
+    name: 'LESS',
+    icon: less,
+  },
+  {
+    name: 'Leaflet',
+    icon: leaflet,
+  },
+  {
+    name: 'JQuery',
+    icon: jquery,
+  },
+  {
+    name: 'Jotform',
+    icon: jotform,
+  },
+  {
+    name: 'Jest',
+    icon: jest,
+  },
+  {
+    name: 'Hotjar',
+    icon: hotjar,
+  },
+  {
+    name: 'Gulp',
+    icon: gulp,
+  },
+  {
+    name: 'Graphql',
+    icon: graphql,
+  },
+  {
+    name: 'Gitlab',
+    icon: gitlab,
+  },
+  {
+    name: 'Github',
+    icon: github,
+  },
+  {
+    name: 'Git',
+    icon: git,
+  },
+  {
+    name: 'Google maps Api',
+    icon: gMaps,
+  },
+  {
+    name: 'Google docs Api',
+    icon: gDosc,
+  },
+  {
+    name: 'Formik',
+    icon: formik,
+  },
+  {
+    name: 'Figma',
+    icon: figma,
+  },
+  {
+    name: 'Docker',
+    icon: docker,
+  },
+  {
+    name: 'Directus',
+    icon: directus,
+  },
+];
